3c92dcdf6f98be973449f625e87d99834cba99bf
Marino Burillo <mburillonavarro@gmail.com>
2023-09-06 15:05:32 +0200

    work please

--- src/rutas/RutaPrivada.jsx
+++ src/rutas/RutaPrivada.jsx
@@ -2,13 +2,10 @@ import { Navigate, useLocation } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 export const RutaPrivada = ({ children }) => {
 	const [isLogged, setState] = useState([])
-
 	useEffect(() => {
-		if (localStorage.getItem('login')) {
-			setState(true)
-		} else {
-			setState(false);
-		}
-	}, []);
+		const loginStatus = localStorage.getItem('login');
+		setState(!!loginStatus);
+	  }, []);
+	  
 	return isLogged ? children : <Navigate to='/login' />;
 };
--- src/rutas/RutaPublica.jsx
+++ src/rutas/RutaPublica.jsx
@@ -2,13 +2,10 @@ import { Navigate, useLocation } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 export const RutaPublica = ({ children }) => {
 	const [isLogged, setState] = useState(false)
-
 	useEffect(() => {
-		if (localStorage.getItem('login')) {
-			setState(true)
-		} else {
-			setState(false);
-		}
-	}, []);
+		const loginStatus = localStorage.getItem('login');
+		setState(!!loginStatus);
+	  }, []);
+	  
 	return isLogged ? <Navigate to='/' /> : children;
 };
